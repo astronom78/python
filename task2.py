88213d6853e9f834d4e656096bb35934b47a5ee4
astronom78 <lizok3546@gmail.com>
2024-10-07 21:12:08 +0300

    lalalla

---
+++ task2.py
@@ -0,0 +1,30 @@
+# todo: Преобразуйте переменную age и foo в число
+# age = "23"
+# foo = "23abc"
+
+age = "23"
+foo = "23abc"
+print(int(age)) #что делать с foo?
+
+# Преобразуйте переменную age в Boolean
+# age = "123abc"
+age2 = "123abc"
+print(bool(age2))
+
+# Преобразуйте переменную flag в Boolean
+# flag = 1
+flag = 1
+print(bool(flag))
+# Преобразуйте значение в Boolean
+# str_one = "Privet"
+# str_two = ""
+str_one = "Privet"
+str_two = ""
+print(bool(str_one), bool(str_two))
+# Преобразуйте значение 0 и 1 в Boolean
+a = 1
+b = 0
+print(bool(a), bool(b))
+# Преобразуйте False в строку
+a = False
+print(str(a))
---
+++ task3.py
@@ -0,0 +1,10 @@
+# todo: Данные две переменные:
+# age = 36.6
+# temperature = 25
+# Нужно обменять значения переменных местами. В итого age
+# должен равнятся 25 а temperature – 36.6:
+
+age = str(input("введите число "))
+temperature = str(input("введите число 3"))
+
+print(f"age is {temperature}, temperature is {age}")
